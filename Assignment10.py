f3ee13b9839bcf10407ac1872e27e4ea71966693
HRRostami <hosseinreza@gmail.com>
2024-11-05 16:23:12 -0500

    finall

--- Assignment10.py
+++ Assignment10.py
@@ -2,4 +2,7 @@ age = input("Are you a cigarette addict older than 75 years old? Yes/No   ").cap
 chronic = input("Do you have a severe chronic disease? Yes/No   ").capitalize() == "Yes"
 immune = input("Is your immune system too weak? Yes/No   ").capitalize() == "Yes"
 risk = age or chronic or immune
-print("Risk of death:", risk)
+if risk:
+    print("You are in risky group")
+else:
+    print("You are not in risky group")
